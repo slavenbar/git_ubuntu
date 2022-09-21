1f6ebb02a86eee6e97f5c691aab63ff57538a51f
slavenbar <gazflair3d@gmail.com>
2022-09-21 22:22:11 +0300

    Create home page with bootstrap

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return "Hello Slava!";
+    return view("home");
+});
+
+Route::get('/about', function () {
+    return view("about");
+});
+
+Route::get('/user/{id}/{name}', function ($id,$name) {
+    return "ID:" . $id."Name:" . $name;
 });
